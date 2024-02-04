84893dd6c9354d269b412ca42c0ecc167f4228e9
Alex <rozenrol@mail.ru>
2024-02-04 17:10:54 +0800

    first version

--- main.cpp
+++ main.cpp
@@ -14,13 +14,54 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories, const string& file_name){
+    path parent = in_file.parent_path();//для поиска в этой дериктории
+    vector<path> directories {include_directories.begin(),include_directories.end()};
+    directories.push_back(parent);
+    static regex num_reg (R"/(\s*#\s*include\s*"([^"]*)"\s*)/");
+    static regex num_reg2 (R"/(\s*#\s*include\s*<([^>]*)>\s*)/");
+    smatch m;
+
+    fstream fout(in_file, ios::in);
+     if (fout.is_open()){
+        string buff;
+        size_t line=1;
+        while (getline(fout, buff)){
+            path new_path;
+            size_t count = 0;
+            if ( regex_match(buff, m, num_reg) || regex_match(buff, m, num_reg2)){
+                
+                for (auto dir=directories.rbegin(); dir!=directories.rend(); dir++){
+                    path pare = *dir;
+                    new_path = path(pare.string() + '/' + static_cast<string>(m[1]));
+                    if (!Preprocess(new_path,out_file, include_directories, file_name)){
+                        count++;        
+                    }
+                }
+                if (count >= directories.size()){
+                    cout<<"unknown include file "<<new_path.filename().string()<< " at file "<<in_file.string()<<" at line " <<line<<'\n' ;
+                    return false;
+                }
+            } else {  
+                ofstream file_to(out_file, ios::app);
+                file_to<<buff<<'\n'; 
+            }
+            line++;
+        }
+    }else{
+        return false;
+    }
+    return true;
+}
+
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories){    
+    bool answer=Preprocess(in_file, out_file, include_directories, in_file.filename().string());
+    return answer;
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
-
-    // конструируем string по двум итераторам
     return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
 }
 
@@ -69,7 +110,8 @@ void Test() {
         ofstream file("sources/include2/lib/std2.h");
         file << "// std2\n"s;
     }
-
+Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
+                                  {"sources"_p / "include1"_p,"sources"_p / "include2"_p});
     assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
                                   {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
 
